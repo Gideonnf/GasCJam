0a4f4284794cac2119f10b69c39a1d677f259cae
Angie <angielimenqi7@gmail.com>
2021-01-05 03:11:05 +0800

    - cross fade transition - code for scene transition and activating the fade transition

---
+++ GasCJam/Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator m_Transition;
+    public float m_TransitionTime;
+
+    void TransitionScene(string sceneName)
+    {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        m_Transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(m_TransitionTime);
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
