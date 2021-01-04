cea83b34fc64279a17829c32267441800b93b25b
Gideon Francis <gideonnfrancis999@gmail.com>
2021-01-05 01:36:01 +0800

    Testing Kitten and Prey Behaviour

---
+++ GasCJam/Assets/Scripts/Behaviour/Detection.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class Detection : MonoBehaviour
+{
+    [Header("Detection settings")]
+    [Tooltip("Detection Radius")]
+    [SerializeField] GameObject CircleRangeCollider;
+    [SerializeField] float CircleRadius = 1;
+
+    [Tooltip("Line of Sight Distance?")]
+    [SerializeField] float SightDistance;
+
+    public bool testingBoolean = false;
+
+    protected List<GameObject> ObjectsInRange = new List<GameObject>();
+
+    public enum DIRECTIONS
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+        NONE
+    }
+
+    /// <summary>
+    /// Detect Colliders around the object
+    /// </summary>
+    /// <returns>true if there is objects found</returns>
+    public virtual bool DetectRadius()
+    {
+        Debug.Log("parent function does a thing");
+
+        // Get a list of all the colliders within the area
+        Collider2D[] ListOfColliders = Physics2D.OverlapCircleAll(transform.position, CircleRadius);
+        
+        // Scan through the list of collided objects to find the game objects
+        foreach (Collider2D collider in ListOfColliders)
+        {
+            // Add it into the list if it isnt the environment
+            // the only objects that would end up here are player, kitten, rats
+            if (collider.gameObject.tag != "Environment")
+            {
+                ObjectsInRange.Add(collider.gameObject);
+            }
+        }
+
+        // if they have found objects in range
+        // then return true
+        if (ObjectsInRange.Count > 0)
+            return true;
+
+        return false;
+    }
+
+    //TODO: idk how do this yet
+    // i'll cross this bridge when i get there
+    public virtual bool DetectInView()
+    {
+
+
+        return false;
+    }
+
+
+}
---
+++ GasCJam/Assets/Scripts/Behaviour/KittenBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittenBehaviour : Detection
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override bool DetectRadius()
+    {
+        // If base detected something in radius
+        if (base.DetectRadius())
+        {
+            //Debug.Log("it detected something useful for the kitten");
+
+        }
+        else
+        {
+
+        }
+            //Debug.Log("Nothing detected on the kitten");
+
+        
+
+        return false;
+    }
+}
