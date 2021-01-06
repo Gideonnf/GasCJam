a54e66c691dc85e44783916b7eaceb45ee6b65dc
Angie <angielimenqi7@gmail.com>
2021-01-06 16:08:20 +0800

    - put in the player cat animation

--- GasCJam/Assets/Scripts/Player/PlayerController.cs
+++ GasCJam/Assets/Scripts/Player/PlayerController.cs
@@ -4,13 +4,48 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D m_RigidBody;
 
+    Vector3 m_Dir;
+
     [Header("Movement data")]
     public float m_MoveSpeed = 1.0f;
+
+    [Header("Visual")]
+    public Animator m_Animator;
 
     // Start is called before the first frame update
     void Awake()
     {
+        m_Dir = Vector3.zero;
         m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        //cant move diagonally
+        m_Dir = Vector3.zero;
+        if (Input.GetButton("Vertical"))
+        {
+            m_Dir = new Vector3(0.0f, Input.GetAxis("Vertical"));
+        }
+        else if (Input.GetButton("Horizontal"))
+        {
+            m_Dir = new Vector3(Input.GetAxis("Horizontal"), 0.0f);
+        }
+
+        UpdateAnimation();
+    }
+
+    public void UpdateAnimation()
+    {
+        if (m_Animator != null)
+        {
+            m_Animator.SetFloat("Horizontal", m_Dir.x);
+            m_Animator.SetFloat("Vertical", m_Dir.y);
+
+            Debug.Log(m_Dir);
+
+            //if direction == vector3.zero, go to idle mode
+        }
     }
 
     //for affecting rigidbody
@@ -21,18 +56,7 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        //cant move diagonally
-        Vector3 dir = Vector3.zero;
-        if (Input.GetButton("Vertical"))
-        {
-            dir = new Vector3(0.0f, Input.GetAxis("Vertical"));
-        }
-        else if (Input.GetButton("Horizontal"))
-        {
-            dir = new Vector3(Input.GetAxis("Horizontal"), 0.0f);
-        }
-
-        dir.Normalize();
-        m_RigidBody.MovePosition(transform.position + dir * m_MoveSpeed * Time.fixedDeltaTime);
+        m_Dir.Normalize();
+        m_RigidBody.MovePosition(transform.position + m_Dir * m_MoveSpeed * Time.fixedDeltaTime);
     }
 }
