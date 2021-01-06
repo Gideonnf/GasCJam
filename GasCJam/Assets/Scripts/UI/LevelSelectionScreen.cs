e341700efa1296b7c8971879b6eca47eb728c06f
Angie <angielimenqi7@gmail.com>
2021-01-06 17:14:11 +0800

    - snow floor tiles

---
+++ GasCJam/Assets/Scripts/UI/LevelSelectionScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectionScreen : MonoBehaviour
+{
+    //store all the available levels UI button
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get from the level manager which level is currently unlocked
+        int levelUnlocked = LevelManager.Instance.m_LevelUnlocked;
+
+        //greyed out the levels that has yet to be unlocked,
+    }
+}
