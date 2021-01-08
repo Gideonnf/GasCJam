aa7911fd60f5c2585da42b2cab8582300034c03b
Gideon Francis <gideonnfrancis999@gmail.com>
2021-01-08 12:48:09 +0800

    Playing with lighting in first level

---
+++ GasCJam/Assets/Scripts/Player/LightFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollow : MonoBehaviour
+{
+    GameObject playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the player object
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerObject == null)
+            return;
+
+        // move the light with the player
+        transform.position = playerObject.transform.position;
+ 
+    }
+}
